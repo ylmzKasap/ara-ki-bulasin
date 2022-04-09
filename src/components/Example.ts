5e433877c87c56ea53bc4bec80ed992f9da7c5be
Yılmaz Kasap <ylmzkasap@outlook.com>
2022-04-10 01:59:15 +0300

    Right now...

--- src/components/Example.ts
+++ src/components/Example.ts
@@ -1,7 +1,7 @@
 export const info = {
   hide: false,
   title: 'ARA Kİ BULASIN',
-  description: 'u Wordle klonunu başkalarıyla canlı oynamak için bağlantıyı paylaşın. Kendinize karşı denemek için yeni bir pencerede açın.',
+  description: 'Bu Wordle klonunu başkalarıyla canlı oynamak için bağlantıyı paylaşın. Kendinize karşı denemek için yeni bir pencerede açın.',
   githubHref: 'https://github.com/CTNicholas/wordle-wars',
   codeSandboxHref: '',
   twitterHref: 'https://twitter.com/@ctnicholasdev',
