4e1e57d5ecc85da241baada9b3149b42e35796d8
Yılmaz Kasap <ylmzkasap@outlook.com>
2024-08-15 23:27:11 +0300

    Change server url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: "http://localhost:3002",
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "")
