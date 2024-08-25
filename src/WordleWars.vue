4b10f8d0a95698060bf26a66bed5d09e16c57546
Yılmaz Kasap <ylmzkasap@outlook.com>
2024-08-25 11:14:58 +0300

    Update server URL

--- src/WordleWars.vue
+++ src/WordleWars.vue
@@ -36,8 +36,9 @@ const room_id = params.get('room');
 
 const maxUsernameLength = 40;
 const savedUsername = localStorage.getItem('username') || '';
-const serverPath = import.meta.env.SERVER_URL;
-const serverUrl = import.meta.env.MODE === 'production' ? serverPath : '/api'
+const serverUrl = import.meta.env.MODE === 'production' 
+  ? 'https://araki-backend-y45yxscdrq-uc.a.run.app/' 
+  : '/api'
 
 let gameState: GameState = $ref(GameState.CONNECTING)
 let username = $ref(savedUsername.length <= maxUsernameLength ? savedUsername : '')
